42d092ce100578162099834c5919f3ac28a79d72
Ayush Singhal <ayushsinghal6789@gmail.com>
2022-06-03 12:43:54 +0530

    no

--- app.js
+++ app.js
@@ -2,9 +2,22 @@ function menuchange() {
     var menubar1 = document.querySelector('#bar1')
     var menubar2 = document.querySelector('#bar2')
     var menubar3 = document.querySelector('#bar3')
-    var ul = document.querySelector('ul')
+    var ul = document.querySelector('#navbar ul')
     menubar1.classList.toggle('bar1change')
     menubar2.classList.toggle('bar2change')
     menubar3.classList.toggle('bar3change')
     ul.classList.toggle('ulchange')
+}
+
+window.onscroll = function() { myFunction() };
+
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+    if (window.pageYOffset >= sticky) {
+        navbar.classList.add("sticky")
+    } else {
+        navbar.classList.remove("sticky");
+    }
 }
